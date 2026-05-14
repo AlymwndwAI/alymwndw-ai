c073fd8f67f15858171f940b71b7a52e97a55b21
AlymwndwAI <alymwndw1@gmail.com>
2026-05-14 20:15:25 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,17 @@
 import express from "express";
-import fetch from "node-fetch";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 
-app.get("/get-token", async (req, res) => {
-  try {
-    const response = await fetch(
-      `https://${process.env.SHOPIFY_STORE}/admin/oauth/access_token`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          client_id: process.env.SHOPIFY_CLIENT_ID,
-          client_secret: process.env.SHOPIFY_CLIENT_SECRET,
-          grant_type: "client_credentials",
-        }),
-      }
-    );
-
-    const data = await response.json();
-
-    console.log(data);
-
-    res.json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Error");
-  }
+app.get("/", (req, res) => {
+  res.send("HOME WORKING");
 });
 
-app.listen(3000, () => {
+app.get("/get-token", (req, res) => {
+  res.send("TOKEN ROUTE WORKING");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Server Running");
 });
