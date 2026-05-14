49d5f393d05877c306161bfb66d1214223e394a9
AlymwndwAI <alymwndw1@gmail.com>
2026-05-14 18:06:43 +0400

    Update makeForYou.js

--- makeForYou.js
+++ makeForYou.js
@@ -1,17 +1,1 @@
-const axios = require("axios");
-
-async function getProducts() {
-  try {
-    const res = await axios.get(
-      "https://alymwndw.com/products.json?limit=250"
-    );
-
-    return res.data.products || [];
-
-  } catch (err) {
-    console.log("Shopify error:", err.message);
-    return [];
-  }
-}
-
-module.exports = { getProducts };
+console.log("Alymwndw AI Loaded");
